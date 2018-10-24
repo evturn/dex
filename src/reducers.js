d65c62d36b44b9e9b44fa0e518007c43855e4028
Evan Turner <ev@evturn.com>
2018-10-23 22:23:22 -0400

    Add created item to store state

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,22 @@
-function rootReducer(state, action) {
+const createID = initializeIDs();
+
+function itemsReducer(state, action) {
+  switch (action.type) {
+
+    case 'CREATE_ITEM':
+      const item = { ...action.item, id: createID() };
+      return { ...state, items: state.items.concat(item) };
+
+  }
+
   return state;
 }
 
-export default rootReducer;
+function initializeIDs(id=0) {
+  return function createID() {
+    id += 1;
+    return id + '';
+  }
+}
+
+export default itemsReducer;

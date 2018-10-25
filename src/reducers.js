5f68f65f1256dad0053054a70896332171fc0a84
Evan Turner <ev@evturn.com>
2018-10-24 22:23:22 -0400

    Search stored items and filter matches

--- src/reducers.js
+++ src/reducers.js
@@ -7,6 +7,30 @@ function itemsReducer(state, action) {
       const item = { ...action.item, id: createID() };
       return { ...state, items: state.items.concat(item) };
 
+    case 'SEARCH_ITEMS':
+      const itemsById = action.query.split(' ').reduce((matched, keyword) => {
+        const word = keyword.toLowerCase();
+        const matches = state.items.filter(item => {
+          const { title, content, id } = item;
+          return title.toLowerCase().includes(word) 
+              || content.toLowerCase().includes(word);
+        });
+
+        if (matches.length) {
+          return matches.reduce((newMatched, match) => {
+            const { id } = match;
+            if (!newMatched[id]) {
+              newMatched[id] = match;
+            }
+            return newMatched;
+          }, matched);
+        }
+        return matched;
+      }, {});
+
+      const itemIds = Object.keys(itemsById);
+      const results = itemIds.map(id => itemsById[id]);
+      return { ...state, results };
   }
 
   return state;
